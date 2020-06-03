6cd9d4ee060ec19b011af89cc6c0c80ac1938091
fellinger-schule <julian.fellinger@gmail.com>
2020-06-03 15:57:59 +0200

    all done

--- src/main/java/GermanStyleDateChecker.java
+++ src/main/java/GermanStyleDateChecker.java
@@ -3,6 +3,10 @@ import java.util.regex.Pattern;
 
 public class GermanStyleDateChecker {
     public static Matcher getMatcher(String dateString) {
-        return null;
+        String patternString = "^(3[01]|[12][0-9]|[1-9])(\\.|-| )(1[0-2]|0?[1-9])(\\.|-| )(19|20)?\\d{2}$";
+
+        Pattern pattern = Pattern.compile(patternString);
+
+        return pattern.matcher(dateString);
     }
 }
